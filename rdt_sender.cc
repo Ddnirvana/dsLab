1d7bd1a94ff34a8421af06fbdab4e2e76823f419
Ddnirvana <ddnirvana1@gmail.com>
2016-03-11 23:26:32 +0800

    add the windows code in the  sender side
    Signed-off-by: Ddnirvana <ddnirvana1@gmail.com>

--- rdt_sender.cc
+++ rdt_sender.cc
@@ -14,6 +14,15 @@
  */
 
 
+/*
+ * packet layout:
+ * |<- 1 byte ->|<- 1 byte ->|<- 1 byte ->|<-  the rest ->|
+ * |payload size|   seq no   |  mesage no |    payload    |
+ *
+ *
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,10 +30,15 @@
 #include "rdt_struct.h"
 #include "rdt_sender.h"
 
-
+static char * dd_windows;
+static long int dd_windSize;
+static int dd_ready;
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
 {
+    dd_windows=NULL;
+    dd_windSize=0;
+    dd_ready=0;
     fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
 }
 
@@ -34,6 +48,22 @@ void Sender_Init()
    memory you allocated in Sender_init(). */
 void Sender_Final()
 {
+    int i=0,j;
+    int size;
+    if (dd_windows) {
+    	while(i<dd_windSize){
+		fprintf(stdout,"Log:mesg:%d-->content:\n",dd_windows[i++]);
+		size=*((int *)(dd_windows+i));
+		for (j=0;j<size;j++){
+			fprintf(stdout,"%c",dd_windows[i++]);
+		}
+	}
+
+    	free(dd_windows);
+    }
+    else {
+    	fprintf(stderr, "opps, windows is null\n");
+    }
     fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
 }
 
@@ -41,6 +71,26 @@ void Sender_Final()
    sender */
 void Sender_FromUpperLayer(struct message *msg)
 {
+    static char messno=0;
+    char * tmpch;
+    if (!dd_ready){
+    	dd_windSize+=msg->size+4+1;
+	if (dd_windows){
+		tmpch=(char *)realloc(dd_windows,dd_windSize);
+		dd_windows=tmpch;
+	}
+	else
+		dd_windows=(char *)malloc(dd_windSize);
+	memcpy(dd_windows+dd_windSize-msg->size+4,msg->data,msg->size);
+	*((int*)(dd_windows+dd_windSize-msg->size+1))=msg->size;
+	messno=(messno+1) & 0xff;
+	*(dd_windows+dd_windSize-msg->size) = messno;
+//	if (messno==0) 
+	dd_ready=1;
+	return;
+    }
+
+#if 0
     /* 1-byte header indicating the size of the payload */
     int header_size = 1;
 
@@ -76,10 +126,12 @@ void Sender_FromUpperLayer(struct message *msg)
 	/* send it out through the lower layer */
 	Sender_ToLowerLayer(&pkt);
     }
+#endif
 }
 
 /* event handler, called when a packet is passed from the lower layer at the 
    sender */
+/* Dd: use to receviced the ack package */
 void Sender_FromLowerLayer(struct packet *pkt)
 {
 }
@@ -87,4 +139,5 @@ void Sender_FromLowerLayer(struct packet *pkt)
 /* event handler, called when the timer expires */
 void Sender_Timeout()
 {
+	
 }
